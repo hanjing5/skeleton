9c86f8328ddb4eb858ffdf37b81f310e5c34d4ff
Han <hjing@dhcp-nyc-658.redrover.cornell.edu>
2017-10-02 21:36:20 -0400

    update the receipt to return the correct merchant and amount

--- src/main/java/controllers/ReceiptImageController.java
+++ src/main/java/controllers/ReceiptImageController.java
@@ -6,6 +6,7 @@ import com.google.protobuf.ByteString;
 import java.math.BigDecimal;
 import java.util.Base64;
 import java.util.Collections;
+import java.util.ArrayList;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import org.hibernate.validator.constraints.NotEmpty;
@@ -25,6 +26,9 @@ public class ReceiptImageController {
 
     }
 
+    public boolean isNumeric(String s) {  
+        return s != null && s.matches("[-+]?\\d*\\.?\\d+");  
+    }
     /**
      * This borrows heavily from the Google Vision API Docs.  See:
      * https://cloud.google.com/vision/docs/detecting-fulltext
@@ -38,26 +42,71 @@ public class ReceiptImageController {
      */
     @POST
     public ReceiptSuggestionResponse parseReceipt(@NotEmpty String base64EncodedImage) throws Exception {
+        // System.out.println(base64EncodedImage);
         Image img = Image.newBuilder().setContent(ByteString.copyFrom(Base64.getDecoder().decode(base64EncodedImage))).build();
         AnnotateImageRequest request = this.requestBuilder.setImage(img).build();
 
         try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+            System.out.println("google created the client");
             BatchAnnotateImagesResponse responses = client.batchAnnotateImages(Collections.singletonList(request));
             AnnotateImageResponse res = responses.getResponses(0);
 
             String merchantName = null;
             BigDecimal amount = null;
 
+            System.out.println("iterating to get some meta data");
             // Your Algo Here!!
             // Sort text annotations by bounding polygon.  Top-most non-decimal text is the merchant
             // bottom-most decimal text is the total amount
+
+            ArrayList<String> amounts = new ArrayList<String>();
+            ArrayList<String> merchants = new ArrayList<String>();
+
             for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
-                out.printf("Position : %s\n", annotation.getBoundingPoly());
-                out.printf("Text: %s\n", annotation.getDescription());
+                // System.out.printf("Position : %s\n", annotation.getBoundingPoly());
+                // System.out.printf("Text: %s\n", annotation.getDescription());
+                System.out.println();
+                String currentText = annotation.getDescription();
+                System.out.println(currentText);
+                System.out.println(isNumeric(currentText));
+
+                if (isNumeric(currentText)) {    
+                    amounts.add(currentText);
+                } else {
+                    merchants.add(currentText);                    
+                }
             }
+            System.out.println("Do we exit the loop first?");
+
+            System.out.println(amounts);
+            System.out.println(merchants);
+
+            if (amounts.size() > 0) {
+                amount = new BigDecimal(amounts.get(amounts.size() - 1));
+            }
+            
+            if (merchants.size() > 0) {
+                merchantName = merchants.get(1); // grab the second element because the first element is everything joined by the newline character
+                System.out.println();
+                System.out.println("This is the first element from merchants arraylist");
+                System.out.println(merchantName);
+                System.out.println();
+            }
+        
+            System.out.println("assigned everything to variables");
+            System.out.println(merchants.size());
+            // System.out.println(annotation.getDescription();
+            System.out.println("Merchant: " + merchantName);
+            System.out.println(amounts.size());
+            System.out.println("Amount: " + amount.toString());
+            System.out.println("iterating the second time to print everything");
+            //   // For full list of available annotations, see http://g.co/cloud/vision/docs
+            //   for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
+            //     annotation.getAllFields().forEach((k, v) -> System.out.printf("%s : %s\n", k, v.toString()));
+            // }
 
             //TextAnnotation fullTextAnnotation = res.getFullTextAnnotation();
             return new ReceiptSuggestionResponse(merchantName, amount);
-        }
+        } 
     }
 }
